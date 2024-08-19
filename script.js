9cdc9b6318982308a2eeb9eb43e7cf2571f17d6f
AvThirtySix <abelvantrigt@hotmail.com>
2024-08-19 23:45:20 +0200

    added files

---
+++ script.js
@@ -0,0 +1,51 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  let x = document.getElementById("mytab");
+  if (x.className === "tab") {
+    x.className += " responsive";
+  } else {
+    x.className = "tab";
+  }
+}
+
+/* slideshow script */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+/* FAQ SCRIPT */
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+/* Footer script */
+n =  new Date();
+y = n.getFullYear();
+document.getElementById("year").innerHTML = y;
