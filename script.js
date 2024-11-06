13449172ad7bebfea58b8d3ebd63c7c1560d9b2b
AvThirtySix <abelvantrigt@hotmail.com>
2024-11-06 10:14:25 +0100

    some changes made

--- script.js
+++ script.js
@@ -56,7 +56,14 @@ function emailSend(){
 );
 }
 
-
+/* google maps script */
+function myMap() {
+  let mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
 
 /* Footer script */
 n =  new Date();
